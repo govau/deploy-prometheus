59b48ef761c094d11a0e674e6e11b1a2b39aa9b3
Brendan Gordon <brendan.gordon@digital.gov.au>
2018-01-09 13:27:30 +1100

    Fetch dns values from each env

--- ci/create-pipeline.sh
+++ ci/create-pipeline.sh
@@ -7,15 +7,22 @@ CREDENTIALS=credentials.yml
 
 if [[ ${TARGET} == "" ]]; then
   TARGET=local
-fi
-
-# Use target-specific credentials file if available
-if [[ -f credentials-${TARGET}.yml ]]; then
-  CREDENTIALS=credentials-${TARGET}.yml
+  # Use target-specific credentials file if available
+  if [[ -f credentials-local.yml ]]; then
+    CREDENTIALS=credentials-local.yml
+  fi
 fi
 
 fly validate-pipeline --config pipeline.yml
 
-fly --target ${TARGET} set-pipeline --config pipeline.yml --pipeline prometheus -n -l $CREDENTIALS
+fly --target ${TARGET} set-pipeline --config pipeline.yml --pipeline prometheus -n \
+  -l $CREDENTIALS \
+  -l credentials-root-cld.yml \
+  -l credentials-b-cld.yml \
+  -l credentials-d-cld.yml \
+  -l credentials-g-cld.yml \
+  -l credentials-y-cld.yml
+
+fly -t ${TARGET} check-resource --resource prometheus/deploy-prometheus.git
 
 fly -t ${TARGET} unpause-pipeline -p prometheus
--- ci/scripts/generate-dns-values.sh
+++ ci/scripts/generate-dns-values.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
 set -e
+set -v
+
+: "${JUMPBOX:?Need to set JUMPBOX e.g. bosh-jumpbox.d.foo}"
+: "${JUMPBOX_SSH_KEY:?Need to set JUMPBOX_SSH_KEY}"
+
+PATH_TO_KEY=${PWD}/jumpbox.pem
+
+# Create the private key for the jumpbox
+echo "${JUMPBOX_SSH_KEY}">${PATH_TO_KEY}
+chmod 600 ${PATH_TO_KEY}
+
+SSH="ssh -i ${PATH_TO_KEY} ec2-user@${JUMPBOX}"
+
+# preload the jumpbox host key
+mkdir ~/.ssh
+KNOWN_HOSTS=~/.ssh/known_hosts
+ssh-keyscan -H ${JUMPBOX} > ${KNOWN_HOSTS}
 
 test $DNS_VAR_FILE
 
-PROMETHEUS_EXTERNAL_HOSTNAME=$(dig +short public.prometheus.monitoring.cld.internal ptr)
+PROMETHEUS_EXTERNAL_HOSTNAME=$($SSH dig +short public.prometheus.monitoring.cld.internal ptr)
 PROMETHEUS_EXTERNAL_HOSTNAME=${PROMETHEUS_EXTERNAL_HOSTNAME:0:-1} # Drop trailing period
-PROMETHEUS_EXTERNAL_IP=$(dig +short ${PROMETHEUS_EXTERNAL_HOSTNAME})
+PROMETHEUS_EXTERNAL_IP=$($SSH dig +short ${PROMETHEUS_EXTERNAL_HOSTNAME})
 
-ALERTMANAGER_EXTERNAL_HOSTNAME=$(dig +short public.alertmanager.monitoring.cld.internal ptr)
+ALERTMANAGER_EXTERNAL_HOSTNAME=$($SSH dig +short public.alertmanager.monitoring.cld.internal ptr)
 ALERTMANAGER_EXTERNAL_HOSTNAME=${ALERTMANAGER_EXTERNAL_HOSTNAME:0:-1} # Drop trailing period
-ALERTMANAGER_EXTERNAL_IP=$(dig +short ${ALERTMANAGER_EXTERNAL_HOSTNAME})
+ALERTMANAGER_EXTERNAL_IP=$($SSH dig +short ${ALERTMANAGER_EXTERNAL_HOSTNAME})
 
-GRAFANA_EXTERNAL_HOSTNAME=$(dig +short public.grafana.monitoring.cld.internal ptr)
+GRAFANA_EXTERNAL_HOSTNAME=$($SSH dig +short public.grafana.monitoring.cld.internal ptr)
 GRAFANA_EXTERNAL_HOSTNAME=${GRAFANA_EXTERNAL_HOSTNAME:0:-1} # Drop trailing period
-GRAFANA_EXTERNAL_IP=$(dig +short ${GRAFANA_EXTERNAL_HOSTNAME})
+GRAFANA_EXTERNAL_IP=$($SSH dig +short ${GRAFANA_EXTERNAL_HOSTNAME})
 
+ENV_NAME=$($SSH sdget env.cld.internal name)
+
+# bosh_exporter 3.0.0 properly validates the director tls cert, but it currently
+# only contains the ip address in the cert SAN. So we have to connect using
+# its IP address until we can fix the director tls cert to include the domain
+# FIXME change back to director.bosh.cld.internal
+BOSH_TARGET=$($SSH dig +short director.bosh.cld.internal)
 
 cat > $DNS_VAR_FILE <<END_OF_OPS
 ---
@@ -27,6 +51,8 @@ alertmanager_external_hostname: $ALERTMANAGER_EXTERNAL_HOSTNAME
 
 grafana_external_ip: $GRAFANA_EXTERNAL_IP
 grafana_external_hostname: $GRAFANA_EXTERNAL_HOSTNAME
+
+${ENV_NAME}_bosh_target: $BOSH_TARGET
 
 END_OF_OPS
 
